e36c3d5ad267276c6d4052c9f25f42b83577b344
Rafael Santana <rafaelsantana7213@gmail.com>
2020-07-14 16:43:40 -0300

    Updates

---
+++ precache-manifest.95126641972b7123704434fdbaf63975.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad0445d7c9361e6eb861d35301c8288d",
+    "url": "/biscoitoSorte/index.html"
+  },
+  {
+    "revision": "36cd321b1788f7422999",
+    "url": "/biscoitoSorte/static/css/main.0aa58293.chunk.css"
+  },
+  {
+    "revision": "929b811748f85df4863c",
+    "url": "/biscoitoSorte/static/js/2.fa773770.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/biscoitoSorte/static/js/2.fa773770.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36cd321b1788f7422999",
+    "url": "/biscoitoSorte/static/js/main.f9d1e42e.chunk.js"
+  },
+  {
+    "revision": "70a6fc6a043de414f48c",
+    "url": "/biscoitoSorte/static/js/runtime-main.6797994b.js"
+  },
+  {
+    "revision": "a2bd231ca8e65322172513b0b27e00b7",
+    "url": "/biscoitoSorte/static/media/biscoito.a2bd231c.png"
+  }
+]);
